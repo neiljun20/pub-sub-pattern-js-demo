d93131c828d9a3029c369fd60133c91bf7dea76b
Neiljun Cataag <neil@deskma.com>
2023-02-04 01:30:02 +0800

    add fire, listen, and unsubsribe code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const EventBusFactory = () => {
     const listeners = {};
 
     const fire = (event, payload) => {
-        // publish message here
+        // publish an event listener
+        return listeners[event].map(callback => callback(payload));
     };
 
     const listen = (event, listener) => {
-        // subscribe a message
+        // create an event listener
+        if (!listeners[event]) listeners[event] = [];
+        listeners[event].push(listener);
     };
 
     const unsubscribe = (event, removeListener) => {
-        // unsubscribe from event
+        // unsubscribe a listener from event
+        listeners[event] = listeners[event].filter((listener) => listener !== removeListener);
     };
 
     return {
